ec4b8a9123a9ab02aa7edc32d04a7bebacc608f6
Aankit Singh <52100296+annkit-singh@users.noreply.github.com>
2020-09-16 20:26:18 +0530

    Update UltraFast.java
    Fast Input output  for Competitive Programming

--- UltraFast.java
+++ UltraFast.java
@@ -78,3 +78,80 @@ System.out.println();
       
     }
 }
+
+//Separate UltraFast
+ import java.nio.charset.StandardCharsets;
+class UltraFast {
+      BufferedReader br;
+      StringTokenizer st;
+
+      public UltraFast() {
+        BufferedInputStream bis = new BufferedInputStream(System.in);
+         br = new BufferedReader(new InputStreamReader(bis, StandardCharsets.UTF_8));
+      }
+
+      String next() {
+        while (st == null || !st.hasMoreElements()) {
+          try {
+            st = new StringTokenizer(br.readLine());
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+        return st.nextToken();
+      }
+
+      int nextInt() {
+        return Integer.parseInt(next());
+      }
+
+      long nextLong() {
+        return Long.parseLong(next());
+      }
+
+      double nextDouble() {
+        return Double.parseDouble(next());
+      }
+
+      String nextLine() {
+        String str = "";
+        try {
+          str = br.readLine();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        return str;
+      }
+
+      int[] readintarray(int n) {
+        int res[] = new int[n];
+        for (int i = 0; i < n; i++)
+          res[i] = nextInt();
+        return res;
+      }
+
+      Integer[] readIntegerArray(int n) {
+        Integer res[] = new Integer[n];
+        for (int i = 0; i < n; i++)
+          res[i] = nextInt();
+        return res;
+      }
+
+      long[] readlongarray(int n) {
+        long res[] = new long[n];
+        for (int i = 0; i < n; i++)
+          res[i] = nextLong();
+        return res;
+      }
+
+      Long[] readLongArray(int n) {
+        Long res[] = new Long[n];
+        for (int i = 0; i < n; i++)
+          res[i] = nextLong();
+        return res;
+      }
+    }
+
+   
+    
+
